0fddb2211fae5787719b49200e52635626569058
rusucosmin <cr.rusucosmin@gmail.com>
2015-10-05 23:07:00 +0300

    Updated homework

--- fop/fop_lab01_29092015/ex9-homework.py
+++ fop/fop_lab01_29092015/ex9-homework.py
@@ -1,24 +1,60 @@
-contains = True;
 
-while contains:														#while the string still contains other characters than digits
-	contains = False;
-	a = input("please insert the number " )		#get the number as a string
-	for x in a:
-		if not x in "0123456789":
-			contains = True
-		
-fr = [0 for i in range(0, 10)]
-for x in a:
-	fr[ord(x) - ord('0')] += 1
+def solveCase2(l):
+	if l == [0]:
+		return [0]
+	# do a count sort
+	fr = [0 for i in range(0, 10)]
 
-for x in range(1, 10):
-	if fr[x] != 0:
-		first = x
-		break
+	for x in l:
+		fr[x] += 1
 
-fr[first] -= 1
+	for x in range(1, 10):
+		if fr[x] != 0:
+			first = x
+			break
+	fr[first] -= 1
+	ret = []
+	ret.append(first)
+	for x in range(0, 10):
+		for y in range (0, fr[x]):
+			ret.append(x)
+	return ret
 
-print(first, end='')
-for x in range(0, 10):
-	for i in range(0, fr[x]):
-		print(x, end='')
+def extractDigits(x):  #functions that returns a list containing the digits of a number
+	digits = []
+	if x == 0:
+		return [0]
+	while x > 0:
+		digits.append(x % 10)
+		x = x // 10
+	return digits
+	
+def readInput():  # function that reads input and returns an integer or None if the user did not type a number
+	try:
+		ret = int(input("Please input a natural number "))
+		if ret >= 0:
+			return ret
+		else: 
+			print("You did not enter a valid number.")
+			return None
+	except:
+		print("You did not enter a valid number.")
+	return None
+
+def solveCase1(digits): #function that returns the nondecreasing sorted list of list "digits"
+	return sorted(digits)
+
+def main():
+	n = None
+	while n is None:
+		n = readInput()
+	digits = extractDigits(n)
+	#ambiguity in the problem statement
+	print ("Minimal number with beginning 0:")
+	print (''.join(str(x) for x in solveCase1(digits)))
+	print("Minimal number without beginnings 0:")
+	print (''.join(str(x) for x in solveCase2(digits)))
+
+
+if __name__ == '__main__':
+	main()
