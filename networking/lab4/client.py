ec80e13fcf821de8058b62b7a8645fda09a13751
cosmin <cr.rusucosmin@gmail.com>
2016-10-26 11:17:16 +0300

    end of lab

--- networking/lab4/client.py
+++ networking/lab4/client.py
@@ -1,12 +1,16 @@
 import socket
+import sys
 
 UDP_IP = "127.0.0.1"
+#UDP_IP = socket.gethostbyname("linux.scs.ubbcluj.ro")
+print(UDP_IP)
 UDP_PORT = 2222
+
+l = sys.argv[1:]
+print(l)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_addr = (UDP_IP, UDP_PORT)
-
-l = [1, 2, 3, 4, 5];
 
 sock.sendto(str(len(l)), server_addr)
 data, server = sock.recvfrom(1024)
@@ -16,8 +20,8 @@ new_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 new_server_addr = (UDP_IP, int(data))
 
 for x in l:
-    sock.sendto(str(x), new_server_addr)
+    new_sock.sendto(str(x), new_server_addr)
 
-new_data, new_server = sock.recvfrom(1024)
+new_data, new_server = new_sock.recvfrom(1024)
 print("server %s responded %s" % (str(new_server), new_data))
 
