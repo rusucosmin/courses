c20815f9677edaa513cba97e03cbbbddc33836a7
Cosmin <cr.rusucosmin@gmail.com>
2017-05-10 17:08:06 +0300

    updated for vlad

--- ai/lab4.py
+++ ai/lab4.py
@@ -3,21 +3,35 @@
 #pyramid from the cubes in such a way that it has 'stability' (there is not a bigger cube
 #over a smaller one) and there are not two consecutive cubes of the same color.
 
-from random import random, randint
+from random import random, randint, shuffle
 from copy import deepcopy
+from math import exp
+import numpy as np
+
+W = 0.5
+C1 = 0.5
+C2 = 1.5
+
+def s(v):
+    return 1 / (1 + exp(-v))
 
 class Particle:
+    global W, c1, c2
     def __init__(self, n):
-        self.permutation = [randint(0, n - 1) for _ in range(n)]
-        self.velocity = random()
+        self.n = n
+        self.permutation = range(n)
+        shuffle(self.permutation)
+        self.velocity = [random() for _ in range(n)]
         self.fitness = 0
-        self.bestPermutation = 0
+        self.bestPermutation = deepcopy(self.permutation)
         self.bestFitness = 0
+        self.w = W
 
     def evaluate(self, problem):
         self.fitness = 1
-        for i in range(1, n):
-            if problem.s[i - 1] > problem.s[i] and problem.c[i - 1] == problem.c[i]:
+        for i in range(1, self.n):
+            if problem.s[self.permutation[i - 1]] > problem.s[self.permutation[i]] and \
+                    problem.c[self.permutation[i - 1]] == problem.c[self.permutation[i]]:
                 self.fitness += 1
             else:
                 break
@@ -25,32 +39,62 @@ class Particle:
             self.bestFitness = self.fitness
             self.bestPermutation = deepcopy(self.permutation)
 
-    def update(self, particle):
-        raise Exception("Not implemented")
+    def update(self, particle, t):
+        global W, C1, C2
+        for i in range(self.n):
+            if random() < s(self.velocity[i]):
+                this = self.permutation[i]
+                other = particle.permutation[i]
+                for j in range(self.n):
+                    if self.permutation[j] == other:
+                        (self.permutation[i], self.permutation[j]) = (self.permutation[j], self.permutation[i])
+            self.velocity[i] = self.w * self.velocity[i] + \
+                        C1 * random() * (particle.bestPermutation[i] - self.permutation[i]) + \
+                        C2 * random() * (self.bestPermutation[i] - self.permutation[i])
+            self.w = W / (t + 1)
 
 class Swarm:
-    def __init__(self, populationSize):
-        self.v = [Particle(populationSize) for _ in range(populationSize)]
-        self.n = populationSize
+    def __init__(self, swarmSize, n):
+        self.v = [Particle(n) for _ in range(swarmSize)]
+        self.n = swarmSize
 
     def getBestNeighbour(self, particle):
-        raise Exception("Not implemented")
+        return self.getBestParticles(1)[0];
 
-    def getBestParticles(self):
-        raise Exception("Not implemented")
+    def getBestParticles(self, k):
+        self.v.sort(key = lambda part: part.bestFitness)
+        return self.v[-k:]
 
 class Controller:
     def __init__(self, problem):
         self.problem = problem
         self.params = {}
         self.loadParameteres()
-        self.population = Swarm(self.params["initialPopulationSize"])
 
     def iteration(self):
-        raise Exception("Not implemented")
+        for i in range(self.params["swarmSize"]):
+            current = self.swarm.v[i]
+            p = self.swarm.getBestNeighbour(current)
+            current.update(p, i)
+            current.evaluate(self.problem)
 
-    def runAlg(self):
-        raise Exception("Not implemented")
+    def showStat(self):
+        fitness = []
+        bestRuns = []
+        for run in range(self.params["runs"]):
+            print("Running %d" % (run + 1))
+            bestRuns.append(self.runAlg(self.params["bestSize"]))
+            runsBestParticles = map(lambda v: max(v, key = lambda s : s.bestFitness), bestRuns)
+            runsBestFitness = [p.bestFitness for p in runsBestParticles]
+            print("stddev: " + str(np.std(runsBestFitness)))
+            print("mean: " + str(np.mean(runsBestFitness)))
+            print("maxi: " + str(max(runsBestFitness)))
+
+    def runAlg(self, topK):
+        self.swarm = Swarm(self.params["swarmSize"], self.problem.n)
+        for i in range(self.params["iterations"]):
+            self.iteration()
+        return self.swarm.getBestParticles(topK)
 
     def loadParameteres(self):
         try:
@@ -72,7 +116,7 @@ class Problem:
     def loadProblem(self, fileName):
         try :
             f = open(fileName, "r")
-            n = int(f.readline())
+            self.n = int(f.readline())
             for line in filter(None, f.read().split('\n')):
                 (x, y) = line.split(' ')
                 self.s.append(x)
@@ -80,6 +124,9 @@ class Problem:
         except Exception as e:
             print("Exception Problem::loadProblem(fileName): " + str(e))
 
-p = Problem("data01.in")
+p = Problem("data02.in")
 ctrl = Controller(p)
+ctrl.showStat()
+#for part in ctrl.runAlg(10):
+#    print(str(part.bestFitness) + " " + str(part.bestPermutation))
 
