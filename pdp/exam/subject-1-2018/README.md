d7ed063a2140d206f9b7d19375ce37f4587a7a00
rusucosmin <cr.rusucosmin@gmail.com>
2018-02-06 14:29:31 +0200

    Added permutations

---
+++ pdp/exam/subject-1-2018/README.md
@@ -0,0 +1,12 @@
+# Exam to Parallel and Distributed Programming `feb 2018, subject no 1`
+
+1. (3p) Write a parallel or distributed program that counts the number of permutations of `N` that satisfy a given property.
+You have a function (`bool pred(vector <int> const& v)`) that
+verifies if a given permutation satisfies the property.
+Your program shall call that function once for each permutation
+and count the number of times it returns true.
+
+see: `permute_async.cpp`
+
+
+
---
+++ pdp/exam/subject-1-2018/permute_async.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+bool check(vector <int> v) {
+  if(v[0] % 2 == 0) {
+    return true;
+  }
+  return false;
+}
+
+bool contains(vector <int> v, int n) {
+  for(auto it : v) {
+    if(it == n) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void back(vector <int> sol, int T, int n) {
+  if(sol.size() == n) {
+    if(check(sol)) {
+      for(auto it : sol) {
+        cerr << it << ' ';
+      }
+      cerr << "da\n";
+    }
+    return;
+  }
+  if(T == 1) {
+    for (int i = 1; i <= n; ++ i) {
+      if(contains(sol, i)) continue;
+      sol.push_back(i);
+      back(sol, T, n);
+      sol.pop_back();
+    }
+  } else {
+    vector <int> x(sol);
+    thread t([&](){
+      for(int i = 1; i <= n; i += 2) {
+        if(contains(x, i)) continue;
+        x.push_back(i);
+        back(x, T / 2, n);
+        x.pop_back();
+      }
+    });
+    for(int i = 2; i <= n; i += 2) {
+      if(contains(sol, i)) continue;
+      sol.push_back(i);
+      back(sol, T - T / 2, n);
+      sol.pop_back();
+    }
+    t.join();
+  }
+}
+
+int main() {
+  back(vector <int>(), 2, 3);
+}
